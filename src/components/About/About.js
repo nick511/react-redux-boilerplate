ec486014e02c4b69ed9114698196dd1e34b37daa
Nick <nick@511.pm>
2018-05-12 22:15:16 -0700

    Add Redux

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
-const About = () => {
-  return (
-    <div>
-      About
-    </div>
-  );
+import { fetchZipCodes, fetchZipCodes2 } from '../../state/actions/zipCodeActions'
+
+export class About extends Component {
+  render () {
+    let { fetching, fetched, failed, zipCodes, fetchZipCodes, fetchZipCodes2 } = this.props
+    console.log(fetching, fetched, failed, zipCodes)
+    return (
+      <div>
+        About
+        <hr/>
+        <button onClick={fetchZipCodes}>fetchZipCodes</button>
+        <button onClick={fetchZipCodes2}>fetchZipCodes2</button>
+      </div>
+    )
+  }
 }
 
-export default About
+const mapStateToProps = state => {
+  const { fetching, fetched, failed, zipCodes } = state.zipCodes
+  return { fetching, fetched, failed, zipCodes }
+}
 
+const mapDispatchToProps = dispatch => (
+  bindActionCreators({ fetchZipCodes, fetchZipCodes2 }, dispatch)
+)
+
+export default connect(mapStateToProps, mapDispatchToProps)(About)
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 
+import store from './state/store'
 import registerServiceWorker from './registerServiceWorker'
 import App from './components/App/App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const root = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+
+ReactDOM.render(root, document.getElementById('root'))
 
 registerServiceWorker()
